60638bb4ff369d93de5b328b720f0478aa92076a
cesarrua <cesarxrua@gmail.com>
2022-08-21 14:01:26 -0500

    adding routes

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const app = express();
 const path = require("path");
 
 app.use(express.static("public"));
+
+app.listen(3000, ()=> {
+    console.log("servidor corriendo en el servidor 3000")
+});
+
+app.get(("/home"), (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+
+app.get(("/home/contact"), (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/contact.html"));
+});
