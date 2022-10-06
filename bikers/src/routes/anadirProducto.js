33c29dc5acf13fb3c276a64e3da94f5c0ab860ce
OscarRamos <oscara22a.r@gmail,com>
2022-10-05 20:04:57 -0500

    agregado de la carpeta data

--- bikers/src/routes/anadirProducto.js
+++ bikers/src/routes/anadirProducto.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const mainController = require('../controllers/mainControllers')
+// const multer = require('multer');
+// const path = require('path');
+
+// let storage = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, 'public/images/products')
+//     },
+//     filename: function(req, file, cb){
+//         const newFileName =  Date.now() + path.extname(file.originalname)
+//         cb(null,newFileName);
+//     }
+// })
+
+// let upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', mainController.anadir);
+// router.post('/', upload.any(), homeController.store);
 
 // router.get()
 
