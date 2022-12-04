bf2fe62a1582eb21a4eb8db42a28c83b3c6167ba
OscarRamos <oscara22a.r@gmail,com>
2022-12-04 16:17:30 -0500

    [UPDATE] express-validator registro

--- bikers/src/middleware/middlewareRegistro.js
+++ bikers/src/middleware/middlewareRegistro.js
@@ -1,8 +1,10 @@
 const { body } = require('express-validator');
 
 const validacion = [
-    body('name').exists().notEmpty().withMessage('Debes ingresar un nombre').bail()
-    .isLength({min: 5}).withMessage('El nombre debe tener minimo 5 carácteres')
+    body('name').notEmpty().withMessage('*Debes ingresar un nombre').bail(),
+    body('last_name').notEmpty().withMessage('Debes ingresar un apellido').bail(),
+    body('email').notEmpty().isEmail().withMessage('Debes ingresar un email').bail(),
+    body('confirm_password').notEmpty().withMessage('*Las contraseñas no coinciden')
 ]
 
 module.exports = validacion;
